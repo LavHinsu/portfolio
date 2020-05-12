836823f72b2736a8d1b1f0e9794b6e89dcd7d9e1
Lav-Hinsu <hinsulav105@gmail.com>
2020-05-12 11:46:32 +0530

    migrated to Github.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,30 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import NavbarComponent from '../components/NavbarComponent'
-import axios from 'axios';
+
+var GitHub = require('github-api');
 
 const Project = (props) => {
 
     var read_token = "4dc1ad1eb2d5ed95b4ca5aa7871d17f1b1cad197"
-    const [data, setData] = useState({ hits: [] });
-    //don't bother trying anything with this token, it is read only.
-    useEffect(() => {
+ 
+    var gh = new GitHub({
+           token: read_token
+     });
 
-         // eslint-disable-next-line react-hooks/exhaustive-deps
-        const fetchData = async () => {
-            const result = await
-                axios({
-                    url: 'https://api.github.com/users/Lav-Hinsu',
-                    headers: {
-                        Authorization: 'Bearer ' + read_token
-                    }
-                });
-            setData(result.data);
-        };
-        
-        // eslint-disable-next-line react-hooks/exhaustive-deps  
-        fetchData();
-    }, []);
-    console.log(data)
+    var me = gh.getUser();
+
+    me.listStarredRepos(function(err, repos) {
+        console.log(repos)
+        // look at all the starred repos!
+     });
     return (
         <div>
             <div style={{ fontSize: "large", color: "black" }}>
