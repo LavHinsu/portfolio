2d456613687b68f2ced0a0f7ce2459210a4c2230
Lav-Hinsu <hinsulav105@gmail.com>
2020-05-12 16:01:32 +0530

    added filtering to github api

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,28 +3,43 @@ import NavbarComponent from '../components/NavbarComponent'
 import axios from 'axios';
 
 const Project = (props) => {
-
     var read_token = "4dc1ad1eb2d5ed95b4ca5aa7871d17f1b1cad197"
     const [data, setData] = useState({ hits: [] });
-    //don't bother trying anything with this token, it is read only.
+    //  don't bother trying anything with this token, it has no permissions.
+    //  i add it here because of githubs 30 requests/day rate limit. with this you can go nuts
+    //  on the refresh button
     useEffect(() => {
-
-         // eslint-disable-next-line react-hooks/exhaustive-deps
         const fetchData = async () => {
             const result = await
                 axios({
-                    url: 'https://api.github.com/users/Lav-Hinsu',
+                    url: 'https://api.github.com/users/Lav-Hinsu/repos',
                     headers: {
                         Authorization: 'Bearer ' + read_token
                     }
                 });
             setData(result.data);
         };
-        
-        // eslint-disable-next-line react-hooks/exhaustive-deps  
         fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps  
     }, []);
-    console.log(data)
+
+    function getPortRepos() {
+        var portRepos=[]
+        for (var i = 0; i < data.length; i++) {
+            var obj = data[i];
+            // console.log(obj.description)
+            if (obj.description != null) {
+                if (obj.description.includes("(portfolio)")) {
+                    portRepos.push(obj)
+                }
+            }
+        }
+        // console.log(portRepos)
+        return portRepos
+    }
+    var port_repos = getPortRepos()
+    console.log(port_repos)
+
     return (
         <div>
             <div style={{ fontSize: "large", color: "black" }}>
