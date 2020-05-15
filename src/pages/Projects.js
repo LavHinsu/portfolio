6b30b95cc984e1ffce851f4f690c5cf0867d2dd1
Lav-Hinsu <hinsulav105@gmail.com>
2020-05-15 17:33:03 +0530

    finalized card design

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 // import NavbarComponent from '../components/NavbarComponent'
 import { useDispatch, useSelector } from "react-redux";
 import axios from 'axios';
-import { CardColumns, Card } from 'react-bootstrap';
+import { CardColumns, Card, Badge } from 'react-bootstrap';
 
 const Project = (props) => {
     var read_token = "4dc1ad1eb2d5ed95b4ca5aa7871d17f1b1cad197"
@@ -54,70 +54,117 @@ const Project = (props) => {
         <div>
             <div className="mx-5 mt-3">
                 <CardColumns>
-                    <Card className="text-right">
-                        <blockquote className="blockquote mb-0 card-body">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                                erat a ante.
-                            </p>
-                            <footer className="blockquote-footer">
-                                <small className="text-muted">
-                                    Someone famous in <cite title="Source Title">Source Title</cite>
-                                </small>
+                    <Card className="text-left">
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                <Badge pill variant="primary">
+                                    Primary
+                                </Badge>{' '}</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                             </Card.Text>
+                            <footer className="footer-right">
+                                <Card.Link href="#">View on Github</Card.Link>
                             </footer>
-                        </blockquote>
+                        </Card.Body>
                     </Card>
-                    <Card className="text-right">
-                        <blockquote className="blockquote mb-0 card-body">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                                erat a ante.
-                            </p>
-                            <footer className="blockquote-footer">
-                                <small className="text-muted">
-                                    Someone famous in <cite title="Source Title">Source Title</cite>
-                                </small>
+                    <Card className="text-left">
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                <Badge pill variant="primary">
+                                    Primary
+                                </Badge>{' '}</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                             </Card.Text>
+                            <footer className="footer-right">
+                                <Card.Link href="#">View on Github</Card.Link>
                             </footer>
-                        </blockquote>
+                        </Card.Body>
                     </Card>
-                    <Card className="text-right">
-                        <blockquote className="blockquote mb-0 card-body">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                                erat a ante.
-                            </p>
-                            <footer className="blockquote-footer">
-                                <small className="text-muted">
-                                    Someone famous in <cite title="Source Title">Source Title</cite>
-                                </small>
+                    <Card className="text-left">
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                <Badge pill variant="primary">
+                                    Primary
+                                </Badge>{' '}</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                             </Card.Text>
+                            <footer className="footer-right">
+                                <Card.Link href="#">View on Github</Card.Link>
                             </footer>
-                        </blockquote>
+                        </Card.Body>
                     </Card>
-                    <Card className="text-right">
-                        <blockquote className="blockquote mb-0 card-body">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                                erat a ante.
-                            </p>
-                            <footer className="blockquote-footer">
-                                <small className="text-muted">
-                                    Someone famous in <cite title="Source Title">Source Title</cite>
-                                </small>
+                    <Card className="text-left">
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                <Badge pill variant="primary">
+                                    Primary
+                                </Badge>{' '}</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                             </Card.Text>
+                            <footer className="footer-right">
+                                <Card.Link href="#">View on Github</Card.Link>
                             </footer>
-                        </blockquote>
+                        </Card.Body>
                     </Card>
-                    <Card className="text-right">
-                        <blockquote className="blockquote mb-0 card-body">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                                erat a ante.
-                            </p>
-                            <footer className="blockquote-footer">
-                                <small className="text-muted">
-                                    Someone famous in <cite title="Source Title">Source Title</cite>
-                                </small>
+                    <Card className="text-left">
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                <Badge pill variant="primary">
+                                    Primary
+                                </Badge>{' '}</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                             </Card.Text>
+                            <footer className="footer-right">
+                                <Card.Link href="#">View on Github</Card.Link>
                             </footer>
-                        </blockquote>
+                        </Card.Body>
+                    </Card>
+                    <Card className="text-left">
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                <Badge pill variant="primary">
+                                    Primary
+                                </Badge>{' '}</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                             </Card.Text>
+                            <footer className="footer-right">
+                                <Card.Link href="#">View on Github</Card.Link>
+                            </footer>
+                        </Card.Body>
+                    </Card>
+                    <Card className="text-left">
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                <Badge pill variant="primary">
+                                    Primary
+                                </Badge>{' '}</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                             </Card.Text>
+                            <footer className="footer-right">
+                                <Card.Link href="#">View on Github</Card.Link>
+                            </footer>
+                        </Card.Body>
                     </Card>
                 </CardColumns>
             </div>
